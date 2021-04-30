3f0373859755b1338a54bdadead8d86349a8a5b0
peustef <peustefanis@gmail.com>
2021-04-30 17:15:10 -0300

    Exercício 8 feito

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -82,6 +82,14 @@ function retornaNNumerosPares(n) {
 
 function checaTriangulo(a, b, c) {
   // implemente sua lógica aqui
+  if (a === b && b === c) {
+   return 'Equilátero'
+ } else if (a !== b && b !== c && c !== a) {
+    return 'Escaleno'
+ }
+ else {
+    return 'Isósceles'
+ }
 }
 
 // Exercício 9
