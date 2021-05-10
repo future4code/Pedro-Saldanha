70f6cafe65cfc7c535d7fea98b9c5721aac3305a
peustef <peustefanis@gmail.com>
2021-05-10 20:44:22 -0300

    Parte estática pronta

--- semana6/form-render-cond/src/App.js
+++ semana6/form-render-cond/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import styled from 'styled-components';
 import './App.css';
+import FormRecebido from './components/FormRecebido';
+import Pagina1 from './components/Pagina1';
+import Pagina2 from './components/Pagina2';
+import Pagina3 from './components/Pagina3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainContainer = styled.div `
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+`
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Pagina1 />
+        <Pagina2 />
+        <Pagina3 />
+        <FormRecebido />
+      </MainContainer>
+    );
+  }
 }
-
-export default App;
