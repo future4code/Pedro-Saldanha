4ff3c7d741ba8f32262222c1f39f260ed2d2f515
peustef <peustefanis@gmail.com>
2021-06-09 23:25:17 -0300

    integração get trip detail

--- semana10/labex/src/pages/AdminHomePage.js
+++ semana10/labex/src/pages/AdminHomePage.js
@@ -1,13 +1,32 @@
-import React, { } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useProtectedPage } from '../hooks/useProtectedPage';
 import { goToCreateTrip, goToHome } from '../routes/coordinator';
+import { BASE_URL } from '../constants/urls';
+import { header } from '../constants/headers'
 
 
 function AdminHome() {
   const history = useHistory()
+  const [trip, setTrip] = useState({})
 
   useProtectedPage();
+
+  const getTripDetail = (id) => {
+    axios.get(`${BASE_URL}/trip/${id}`, header)
+      .then((res) => {
+        setTrip(res.data.trip)
+      })
+      .catch((err) => {
+        console.log(err.response.data)
+      })
+
+  }
+
+  useEffect(() => {
+    getTripDetail("67nilY8HZyWMYlVeJxsC")
+  }, [])
 
   const onclickLogout = () => {
     localStorage.removeItem("token")
@@ -17,6 +36,7 @@ function AdminHome() {
   return (
     <div>
       <p>Painel Administrativo</p>
+      <p>{trip.planet}</p>
       <div>
         <button onClick={() => goToCreateTrip(history)}>Criar Viagem</button>
         <button onClick={() => goToHome(history)}>Voltar</button>
