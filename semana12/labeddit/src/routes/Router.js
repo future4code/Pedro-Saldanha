4d2aef8fd4e9d649260d38ae01971d862d88f78b
peustef <peustefanis@gmail.com>
2021-06-21 16:38:30 -0300

    rotas criadas e coordinator funçao para cada página

---
+++ semana12/labeddit/src/routes/Router.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import FeedPage from "../pages/FeedPage/FeedPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import PostDetailPage from "../pages/PostDetailPage/PostDetailPage";
+import SignUpPage from "../pages/SingUpPage/SignUpPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <FeedPage />
+                </Route>
+                <Route exact path="/login">
+                    <LoginPage />
+                </Route>
+                <Route exact path="/cadastro">
+                    <SignUpPage />
+                </Route>
+                <Route exact path="/detalhe/:id">
+                    <PostDetailPage />
+                </Route>
+                <Route>
+                    <ErrorPage />
+                </Route>
+
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
