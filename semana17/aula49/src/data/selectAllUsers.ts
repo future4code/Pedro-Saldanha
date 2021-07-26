86e6ef02a5a957641bdaf16517ee44ee73e0f160
peustef <peustefanis@gmail.com>
2021-07-26 19:29:23 -0300

    ex 1 letra a)

--- semana17/aula49/src/data/selectAllUsers.ts
+++ semana17/aula49/src/data/selectAllUsers.ts
@@ -1,11 +1,12 @@
 import connection from "../connection"
 
 
-export default async function selectAllUsers():Promise<any> {
+export default async function selectAllUsers(name: any): Promise<any> {
     const result = await connection.raw(`
        SELECT id, name, email, type
-       FROM aula49_exercicio;
+       FROM aula49_exercicio
+       WHERE name LIKE '%${name}%';
     `)
- 
+
     return result[0]
- }
+}
